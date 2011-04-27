2e19409c7fa3806c101b161f74f53c7be0194ced
Andreas Ek <andreas@hypernode.se>
2011-04-27 08:13:01 +0200

    Prepp för 2.07

--- tv.boxeeplay.svtplay/start.py
+++ tv.boxeeplay.svtplay/start.py
@@ -4,7 +4,7 @@ import mc, actions, logger
 here we can do some pre launch processing if we wish.
 checking authentication or pre-loading content, anything we need.
 '''
-logger.EnablePlus(logger.Level.DEBUG)
+#logger.EnablePlus(logger.Level.DEBUG)
 
 mc.ActivateWindow(14000)
 actions.initiate()
