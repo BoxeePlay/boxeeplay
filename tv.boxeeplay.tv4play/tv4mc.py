611924d286ff874fc311f047abbbcb93c3b6d28c
Andreas Pehrson <pehrsons@gmail.com>
2011-05-13 13:27:09 +0200

    Put tv4play in its own branch.

---
+++ tv.boxeeplay.tv4play/tv4mc.py
@@ -0,0 +1,57 @@
+﻿#encoding:utf-8
+#author:Mats Boisen
+#project:boxeeplay
+#repository:https://bitbucket.org/hesapesa/boxeeplay
+#license:Creative Commons GNU GPL v2
+# (http://creativecommons.org/license/GPL/2.0/)
+
+import mc
+from urllib import quote_plus
+import tv4xml
+from logger import BPLog,BPTraceEnter,BPTraceExit,Level
+
+def GetCategories() :
+    return tv4xml.GetCategories()
+
+def GetCategoryId(item) :
+    return item.GetProperty("id")
+
+def GetTitles(id) :
+    return tv4xml.GetTitles(id)
+
+def GetTitleId(item) :
+    return item.GetProperty("id")
+
+def GetEpisodes(id) :
+    return tv4xml.GetEpisodes(id)
+
+def GetSamples(id) :
+    return 0
+
+def SearchEpisodes(searchTerm) :
+    return mc.ListItems()
+
+def SearchSamples(searchTerm) :
+    return mc.ListItems()
+
+def DumpAllEpisodes():
+    BPTraceEnter()
+    categories = GetCategories()
+    for category in categories:
+        categoryId = GetCategoryId(category)
+        titles = GetTitles(categoryId)
+        for title in titles:
+            titleId = GetTitleId(title)
+            episodes = GetEpisodes(titleId)
+    BPTraceExit()
+
+def DumpAllSamples():
+    BPTraceEnter()
+    categories = GetCategories()
+    for category in categories:
+        categoryId = GetCategoryId(category)
+        titles = GetTitles(categoryId)
+        for title in titles:
+            titleId = GetTitleId(title)
+            episodes = GetSamples(titleId)
+    BPTraceExit()
