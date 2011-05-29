79f3d64560fbb258980d53ef3dc3ad7f8271e36d
Mats Boisen <mats.boisen@gmail.com>
2011-05-29 16:53:53 +0200

    Started to play with tvseries from res.boxee.tv

--- tv.boxeeplay.tvseries/start.py
+++ tv.boxeeplay.tvseries/start.py
@@ -1,4 +1,12 @@
 ﻿import mc, tvseries, logger
 
 listItems = tvseries.GetChannels()
-id = tvseries.GetChannelId(listItems[0])
+listItems[0].Dump()
+url = tvseries.GetSeriesUrlForChannel(listItems[0])
+mc.LogInfo("tvseries: url- " + url)
+seriesItems = tvseries.GetSeries(url)
+seriesItems[0].Dump()
+url = tvseries.GetEpisodesUrlForSerie(seriesItems[0])
+mc.LogInfo("tvseries: url- " + url)
+episodeItems = tvseries.GetEpisodes(url)
+episodeItems[0].Dump()
--- tv.boxeeplay.tvseries/tvseries.py
+++ tv.boxeeplay.tvseries/tvseries.py
@@ -15,12 +15,24 @@ def GetChannels() :
     BPTraceEnter()
     items = mc.ListItems()
 
+    item0 = mc.ListItem(mc.ListItem.MEDIA_UNKNOWN)
+    item0.SetContentType("text/html")
+    item0.SetLabel("Senaste")
+    item0.SetPath("http://www.boxeeplay.tv")
+    item0.SetThumbnail("http://boxeeplay.tv/wp-content/uploads/2011/04/boxeeplay.png")
+    item0.SetProperty("provider", "")
+    item0.SetProperty("sortBy", "release")
+    item0.SetProperty("count", "100")
+    items.append(item0)
+
     item1 = mc.ListItem(mc.ListItem.MEDIA_UNKNOWN)
     item1.SetContentType("text/html")
     item1.SetLabel("SVT")
     item1.SetPath("http://svtplay.se")
     item1.SetThumbnail("http://dir.boxee.tv/apps/providerthmb/se_svtplay.png")
-    item1.SetProperty("id", "se_svtplay")
+    item1.SetProperty("provider", "se_svtplay")
+    item1.SetProperty("sortBy", "title")
+    item1.SetProperty("count", "1000")
     items.append(item1)
 
     item2 = mc.ListItem(mc.ListItem.MEDIA_UNKNOWN)
@@ -28,7 +40,9 @@ def GetChannels() :
     item2.SetLabel("TV3")
     item2.SetPath("http://www.tv3.se")
     item2.SetThumbnail("http://dir.boxee.tv/apps/providerthmb/se_tv3play.png")
-    item2.SetProperty("id", "se_tv3play")
+    item2.SetProperty("provider", "se_tv3play")
+    item2.SetProperty("sortBy", "title")
+    item2.SetProperty("count", "1000")
     items.append(item2)
 
     item3 = mc.ListItem(mc.ListItem.MEDIA_UNKNOWN)
@@ -36,7 +50,9 @@ def GetChannels() :
     item3.SetLabel("TV4")
     item3.SetPath("http://www.tv4play.se")
     item3.SetThumbnail("http://dir.boxee.tv/apps/providerthmb/se_tv4play.png")
-    item3.SetProperty("id", "se_tv4play")
+    item3.SetProperty("provider", "se_tv4play")
+    item3.SetProperty("sortBy", "title")
+    item3.SetProperty("count", "1000")
     items.append(item3)
 
     item4 = mc.ListItem(mc.ListItem.MEDIA_UNKNOWN)
@@ -44,7 +60,9 @@ def GetChannels() :
     item4.SetLabel("Kanal 5")
     item4.SetPath("http://www.kanal5.se")
     item4.SetThumbnail("http://dir.boxee.tv/apps/providerthmb/se_kanal5.png")
-    item4.SetProperty("id", "se_kanal5")
+    item4.SetProperty("provider", "se_kanal5")
+    item4.SetProperty("sortBy", "title")
+    item4.SetProperty("count", "1000")
     items.append(item4)
 
     item5 = mc.ListItem(mc.ListItem.MEDIA_UNKNOWN)
@@ -52,7 +70,9 @@ def GetChannels() :
     item5.SetLabel("TV6")
     item5.SetPath("http://www.tv6.se")
     item5.SetThumbnail("http://dir.boxee.tv/apps/providerthmb/se_tv6play.png")
-    item5.SetProperty("id", "se_tv6play")
+    item5.SetProperty("provider", "se_tv6play")
+    item5.SetProperty("sortBy", "title")
+    item5.SetProperty("count", "1000")
     items.append(item5)
 
     item6 = mc.ListItem(mc.ListItem.MEDIA_UNKNOWN)
@@ -60,12 +80,22 @@ def GetChannels() :
     item6.SetLabel("TV8")
     item6.SetPath("http://www.tv8.se")
     item6.SetThumbnail("http://dir.boxee.tv/apps/providerthmb/se_tv8play.png")
-    item6.SetProperty("id", "se_tv8play")
+    item6.SetProperty("provider", "se_tv8play")
+    item6.SetProperty("sortBy", "title")
+    item6.SetProperty("count", "1000")
     items.append(item6)
     
     BPTraceExit("Returning %s" % items)
     return items
 
-def GetChannelId(item) :
-    return item.GetProperty("id")
+def GetSeriesUrlForChannel(item) :
+    return "http://res.boxee.tv/titles/tv?adult=yes&count=" + item.GetProperty("count") + "&geo=se&provider=" + item.GetProperty("provider") + "&sort=" + item.GetProperty("sortBy") + "&start=0"
 
+def GetSeries(url):
+    return mc.GetDirectory(url)
+
+def GetEpisodesUrlForSerie(item) :
+    return "http://res.boxee.tv/title/" + item.GetProperty("boxeeid") +  "/episodes?adult=no&geo=se"
+
+def GetEpisodes(url):
+    return mc.GetDirectory(url)
